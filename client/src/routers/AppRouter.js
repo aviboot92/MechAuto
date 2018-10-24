ebcc6787a2dd936ea2f313d19ca63676fbe28f3e
avinash karra <avkarra@gmail.com>
2018-10-23 20:00:35 -0400

    Back TO React

--- client/src/routers/AppRouter.js
+++ client/src/routers/AppRouter.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import PageNotFound from '../components/PageNotFound';
-import Nav from "../components/Nav";
 import Location from "../components/Location";
-import YMMT from "../components/YMMT";
 import Service from "../components/Service";
 
 const AppRouter = () => (
   <BrowserRouter>
     <div>
-        <Nav />
-        <Route path="/" component={Location} />
-        <Route path="/ymmt" component={YMMT} />
+        <Route path="/" component={Location} exact ={true} />
         <Route path="/ymmt/serviceList" component={Service} />
     </div>
   </BrowserRouter>
