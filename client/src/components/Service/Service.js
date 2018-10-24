f1a1fd6122884344aae4b9108000d1450ea55f34
avinash karra <avkarra@gmail.com>
2018-10-24 19:18:40 -0400

    Check Boxes Added

--- client/src/components/Service/Service.js
+++ client/src/components/Service/Service.js
@@ -2,15 +2,22 @@ import React from "react";
 
 
 const Service = (props) =>{
-    let carSer = props.serviceCar[0].service;
-    let serPrice = props.serviceCar[0].price;
+    const services = props.serviceCar;
+    let carSer = services[0].service;
+    let serPrice = services[0].price;
     console.log(`Service: ${carSer} Price: ${serPrice}`);
 
           return(
             <div>
             <h1>Please choose your <b>Service</b> from the list below </h1>
             <ul>
-         
+                {services.map(service =>{
+                    return (<li>
+                        <input type="checkbox" name={service.service} value={service.price}/> <span>{service.service.toUpperCase()} 
+                        </span>: <span> {service.price}</span><br/>
+                          </li>)    
+                }) 
+                }
           </ul>
 
           </div>
@@ -19,7 +26,3 @@ const Service = (props) =>{
 
 export default Service;
 
-// {this.state.listOf.map(item =>{
-//     return <li>{item.service}</li>
-//  }) 
-//  }
